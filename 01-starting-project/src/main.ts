d1b852fd59c58a60456e90e0d1d13a3f1b0d8ba5
Zion Mejia <zion.mejia@ihcs033.nl>
2026-03-09 15:39:36 +0100

    Created a custom component and learned how they interact with eachother.(How to create, How to Import and how to apply them)

--- 01-starting-project/src/main.ts
+++ 01-starting-project/src/main.ts
@@ -3,3 +3,4 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
+
