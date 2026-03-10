e13fdfb6f149cf01fa33d5b3a64f1c4f4e65e9dc
Zion Mejia <zion.mejia@ihcs033.nl>
2026-03-10 11:51:19 +0100

    Learned to define component inputs and how to use required/optional inputs

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import {HeaderComponent} from "./header/header.component";
-import {UserComponent} from "./user/user.component";
+import { HeaderComponent} from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from "./dummy-users";
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,6 @@ import {UserComponent} from "./user/user.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+}
--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import {Component, computed, signal} from '@angular/core';
-import { DUMMY_USERS} from "../dummy-users";
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -12,18 +9,12 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 
 export class UserComponent {
-  public selectedUser = signal(DUMMY_USERS[randomIndex]);
-  protected readonly DUMMY_USERS = DUMMY_USERS;
-  public imagePath = computed(() => "./assets/users/" + this.selectedUser().avatar);
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
 
-
-  // public get imagePath() {
-  //   return './assets/users/' + this.selectedUser().avatar;
-  // }
-
-  public onSelectedUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  public get imagePath(){
+    return './assets/users/' + this.avatar;
   }
 
+  public onSelectUser () {}
 }
