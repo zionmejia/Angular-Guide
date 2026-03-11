300b2bdb907b1e4e37a63a15bb0e82f0b018b16e
Zion Mejia <zion.mejia@ihcs033.nl>
2026-03-11 13:33:46 +0100

    Added missing Component, and cleaned up some of the imports.

--- 03-essentials-modules/01-starting-project/src/app/app.module.ts
+++ 03-essentials-modules/01-starting-project/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import {NgModule} from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
+import {DatePipe} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+
 import {AppComponent} from "./app.component";
 import {HeaderComponent} from "./header/header.component";
 import {UserComponent} from "./user/user.component";
 import {TasksComponent} from "./tasks/tasks.component";
 import {CardComponent} from "./shared/card/card.component";
-import { DatePipe } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import {TaskComponent} from "./tasks/task/task.component";
+import {NewTaskComponent} from "./tasks/new-task/new-task.component";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, UserComponent, TasksComponent, CardComponent, TaskComponent],
-  bootstrap: [AppComponent, HeaderComponent, UserComponent, TasksComponent, CardComponent],
-  imports: [BrowserModule,DatePipe,FormsModule]
+  declarations: [AppComponent, HeaderComponent, UserComponent, TasksComponent, CardComponent, TaskComponent, NewTaskComponent],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, DatePipe, FormsModule]
 })
 export class AppModule {
 
