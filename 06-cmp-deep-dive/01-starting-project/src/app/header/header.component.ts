9023fbc51aad4d6f80b5b2a7c6054e10b562a74b
Zion Mejia <zion.mejia@ihcs033.nl>
2026-03-12 11:54:50 +0100

    Learned about interacting with host element,Css scopes,manipulatin states. This section just deep dives more into concepts that was already explained.

--- 06-cmp-deep-dive/01-starting-project/src/app/header/header.component.ts
+++ 06-cmp-deep-dive/01-starting-project/src/app/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {ButtonComponent} from "../shared/button/button.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonComponent
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
