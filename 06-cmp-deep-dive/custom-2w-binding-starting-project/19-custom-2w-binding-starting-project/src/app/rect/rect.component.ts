5877f43b0fe6ce40190092ff28fa198215155f38
Zion Mejia <zion.mejia@ihcs033.nl>
2026-03-13 14:12:22 +0100

    Finished Section 6.

--- 06-cmp-deep-dive/custom-2w-binding-starting-project/19-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
+++ 06-cmp-deep-dive/custom-2w-binding-starting-project/19-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, input, model, output} from '@angular/core';
 
 @Component({
   selector: 'app-rect',
   standalone: true,
-  imports: [],
   templateUrl: './rect.component.html',
   styleUrl: './rect.component.css',
 })
 export class RectComponent {
-  // Todo: Implement custom two-way binding
+  public size = model.required<{ width: string; height: string }>();
+  public sizeChange =   output<{ width: string; height: string }>();
 
   onReset() {
-    // ...
+    this.size.set({
+      width: '200',
+      height: '100'
+    });
   }
 }
