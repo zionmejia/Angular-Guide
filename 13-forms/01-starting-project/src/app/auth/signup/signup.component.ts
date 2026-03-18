0a06535475a0b13d4ae96a44a135a50d40ceb060
zionmejia <zion_mejia2001@hotmail.com>
2026-03-18 15:29:08 +0100

    Finished Section 13. Learned more about Validators, how to add and build custom ones, interact with form programmatically, nested form groups, multi-input validators

--- 13-forms/01-starting-project/src/app/auth/signup/signup.component.ts
+++ 13-forms/01-starting-project/src/app/auth/signup/signup.component.ts
@@ -1,9 +1,49 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
+  imports: [ReactiveFormsModule]
 })
-export class SignupComponent {}
+export class SignupComponent {
+
+  public form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+      asyncValidators: [],
+    }),
+    passwords: new FormGroup({
+      password: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(6)],
+      }),
+      confirmPassword: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(6)],
+      }),
+    }),
+
+    firstName: new FormControl('', {}),
+    lastName: new FormControl('', {}),
+
+    address: new FormGroup({
+      street: new FormControl('', {}),
+      postcode: new FormControl('', {}),
+      number: new FormControl('', {}),
+      city: new FormControl('', {}),
+    }),
+
+    role: new FormControl<'student' | 'teacher' | 'employee' | 'founder' | 'other'>('student', {}),
+    agree: new FormControl(false, {validators: [Validators.required]}),
+  });
+
+  public onSubmit() {
+    console.log(this.form);
+  }
+
+  public onReset() {
+    this.form.reset();
+  }
+
+}
